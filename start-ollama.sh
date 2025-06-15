9e831e05bb4c708708d52c3275c02bad5e1dc24b
mznluppio <julienmezan@gmail.com>
2025-06-16 01:06:13 +0200

    fix: update ollama

--- start-ollama.sh
+++ start-ollama.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-# Démarre ollama en mode serveur
-ollama serve
+
+# Installer le modèle llama3:8b (ou autre)
+echo "Vérification et installation du modèle llama3.2..."
+ollama pull llama3.2
+
+# Lancer le serveur Ollama
+echo "Lancement du serveur Ollama..."
+exec ollama serve
