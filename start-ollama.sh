255d61ea7411f0e0096e4c87cc724985830e67a6
mznluppio <julienmezan@gmail.com>
2025-06-16 01:00:08 +0200

    fix: ollama

--- start-ollama.sh
+++ start-ollama.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-# Installe le modèle llama3 s'il n'est pas déjà installé
-if ! ollama list | grep -q "llama3.2"; then
-  echo "Modèle llama3.2 non trouvé, installation en cours..."
-  ollama pull llama3.2
-else
-  echo "Modèle llama3.2 déjà installé."
-fi
-
-# Démarre Ollama en mode serveur
+# Démarre ollama en mode serveur
 ollama serve
